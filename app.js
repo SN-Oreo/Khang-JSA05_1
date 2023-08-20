325a5017de0abe7e09c03dd51ac3f231a8f5ecf8
SN-Oreo <duykhange@gmail.com>
2023-08-20 12:16:55 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let arr = ["Toán", "Ngữ Văn", "Tiếng Anh"]
+
+for( let value of arr)
+    console.log(value)
+
+
+// let obj = {
+//     'name': 'Nguyễn Văn Đẩu',
+//     'sex': 'Không xác định',
+//     'crime': 'Phạm vi hành chính'
+// }
