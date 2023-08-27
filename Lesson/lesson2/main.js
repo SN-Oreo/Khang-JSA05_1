dcf91738d9300a351b0cdefe7041e9be86784a9c
SN-Oreo <duykhange@gmail.com>
2023-08-27 12:15:41 +0700

    buoi 4

---
+++ Lesson/lesson2/main.js
@@ -0,0 +1,58 @@
+// let myArray = [4, 5, 7, 8, 9, 1]
+// console.log(myArray)
+
+// myArray[2] = 6;
+// console.log(myArray)
+
+// myArray.splice(2, 1, 7, 8);
+// console.log(myArray)
+
+// console.log(myArray.length)
+
+// myArray.push(7)
+// console.log(myArray)
+
+// myArray.pop()
+// console.log(myArray)
+
+// myArray.unshift(9)
+// console.log(myArray)
+
+// myArray.shift()
+// console.log(myArray)
+
+// let dog = {
+//     name:"Alaska",
+//     age: 15,
+// }
+
+// dog.name ="Chihuahua"
+// console.log(dog)
+
+// dog.action = "walk"
+// console.log(dog)
+
+// delete (dog.action)
+// console.log(dog)
+
+// let cat = new Object({
+//     name = "Tom",
+//     age = 3,
+//     enemy = {
+//         name = "alaska",
+//         age = 5
+//     }
+// }
+// );
+
+// let dog1 = ["Alaska", 4]
+// let dog2 = ["Corgi", 5]
+// let dogs = [dog1, dog2]
+//  console.log(dogs)
+//  console.log(dogs[0][0])
+
+let gioTraiCay = [["Xoai", 2],["Dua hau", 3]];
+
+for (var i = 0; i< gioTraiCay.length; i++){
+    console.log("Giỏ trái cây: " + gioTraiCay[i][0] + " " + gioTraiCay[i][1])
+}
