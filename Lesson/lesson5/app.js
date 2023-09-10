75dbb5e41426f6bb674788413d50234f579c3e93
SN-Oreo <duykhange@gmail.com>
2023-09-10 12:06:29 +0700

    Buoi 5

---
+++ Lesson/lesson5/app.js
@@ -0,0 +1,26 @@
+
+
+const login = document.getElementById("form-login");
+login.addEventListener("submit", function() {
+    // var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if(username === 'username' && password === "password"){
+
+        localStorage.setItem("username", username);
+
+        var greetingElement = document.createElement('p');
+        greetingElement.textContent = "Xin Chào " + username + "!";
+        document.body.appendChild(greetingElement);
+    }else{
+        alert("hmmmm")
+    }
+})
+
+document.addEventListener('DOMContentLoaded', function(){
+    var username = localStorage.getItem('username');
+    if(username){
+        var greetingElement = document.createElement('p');
+        greetingElement.textContent = "Xin Chào " + username + "!";
+        document.body.appendChild(greetingElement);
+    }
+})
